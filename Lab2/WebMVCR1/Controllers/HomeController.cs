afa7cc581b35ff5d46f6791882031981e79acf35
gavrun <38539655+gavrun@users.noreply.github.com>
2025-03-30 13:16:21 +0100

    Added lab 2 item 1

---
+++ Lab2/WebMVCR1/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebMVCR1.Models;
+
+namespace WebMVCR1.Controllers
+{
+    public class HomeController : Controller
+    {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public string Index(string hel)
+        {
+            //string res = ExeEnum();
+
+            string res = ExeStruct();
+
+            return res;
+        }
+
+        public string ExeEnum()
+        {
+            AccountType goldAccount;
+            AccountType platinumAccount;
+
+            goldAccount = AccountType.Checking;
+            platinumAccount = AccountType.Deposit;
+
+            string res1 = String.Format("Bank account type {0}", goldAccount);
+            string res2 = String.Format("Bank account type  {0}", platinumAccount);
+
+            string res = res1 + "<p>" + res2;
+            //string res = "<html><body>" + res1 + "<p>" + res2 + "</body></html>";
+
+            return res;
+        }
+
+        public string ExeStruct()
+        {
+            BankAccount goldBankAccount;
+
+            goldBankAccount.accNo = 123;
+            goldBankAccount.accBal = (decimal)3200.00;
+            goldBankAccount.accType = AccountType.Checking;
+
+            //string res1 = String.Format("Account No {0} balance {1} and type {2}", goldBankAccount.accNo, goldBankAccount.accBal, goldBankAccount.accType);
+
+            //string res = goldBankAccount.ToString();
+            string res = String.Format("Bank account information: {0}", goldBankAccount);
+
+            string res1 = $" [{goldBankAccount.accNo}]";
+
+            return res + res1;
+        }
+    }
+}
---
+++ Lab2/WebMVCR1/Models/StudyCsharp.cs
@@ -0,0 +1,27 @@
+﻿namespace WebMVCR1.Models
+{
+    public class StudyCsharp
+    {
+    }
+
+    public enum AccountType
+    {
+        Checking,
+        Deposit
+    }
+
+    public struct BankAccount
+    {
+        public long accNo;
+        public decimal accBal;
+        public AccountType accType;
+
+        public override string ToString()
+        {
+            string res = String.Format("Account No {0} balance {1} and type {2}", accNo, accBal, accType);
+            
+            return res;
+            //return base.ToString();
+        }
+    }
+}
