bbff053c5ee63b3b2954a672b3135cd02a6a4829
gavrun <38539655+gavrun@users.noreply.github.com>
2025-04-01 23:44:51 +0100

    Added lab 7 item 1

---
+++ Lab7/MvcCreditApp/Models/Credit.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcCreditApp.Models
+{
+    public class Credit
+    {
+        public virtual int CreditId { get; set; }
+
+        [DisplayName("Credit type")]
+        [Required]
+        public virtual string Head { get; set; }
+
+        [Required]
+        public virtual int Period { get; set; }
+
+        [DisplayName("Amount")]
+        [Required]
+        public virtual int Sum { get; set; }
+
+        [DisplayName("Interest rate")]
+        [Required]
+        public virtual int Procent { get; set; }
+
+    }
+}
