db5178ca8e91c9268e89b53556af6727638221fd
gavrun <38539655+gavrun@users.noreply.github.com>
2025-04-01 18:06:15 +0100

    Added Lab 5 item 1

---
+++ Lab5/MvcCreditApp/Models/Bid.cs
@@ -0,0 +1,13 @@
+﻿namespace MvcCreditApp.Models
+{
+    public class Bid
+    {
+        public virtual int BidId { get; set; }
+        
+        public virtual string Name { get; set; }
+        
+        public virtual string CreditHead { get; set; }
+        
+        public virtual DateTime bidDate { get; set; }
+    }
+}
